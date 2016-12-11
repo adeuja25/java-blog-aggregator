55854362eb259efb2aefdf000d133974b140506f
Deuja <Deuja@Deuja>
2016-12-11 15:52:14 -0600

    Final Commit- Application is ready.

--- java-blog-aggregator/src/main/java/cz/jiripinkas/jba/service/InitDbService.java
+++ java-blog-aggregator/src/main/java/cz/jiripinkas/jba/service/InitDbService.java
@@ -1,7 +1,6 @@
 package cz.jiripinkas.jba.service;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import cz.jiripinkas.jba.entity.Blog;
-import cz.jiripinkas.jba.entity.Item;
 import cz.jiripinkas.jba.entity.Role;
 import cz.jiripinkas.jba.entity.User;
 import cz.jiripinkas.jba.repository.BlogRepository;
@@ -23,58 +21,61 @@ import cz.jiripinkas.jba.repository.UserRepository;
 @Transactional
 @Service
 public class InitDbService {
-	@Autowired
-	private RoleRepository roleRepository; 
+		
+		@Autowired
+		private RoleRepository roleRepository; 
+		
+		@Autowired
+		private UserRepository userRepository;
+		
+		@Autowired
+		private ItemRepository itemRepository;
+		
+		@Autowired
+		private BlogRepository blogRepository;
+		
+		@PostConstruct
+		public void init(){
+			if(roleRepository.findByName("ROLE_ADMIN")==null){
 	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private ItemRepository itemRepository;
-	
-	@Autowired
-	private BlogRepository blogRepository;
-	
-	@PostConstruct
-	public void init(){
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		roleRepository.save(roleUser);
-		
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		roleRepository.save(roleAdmin);
-		
-		User userAdmin = new User();
-		userAdmin.setEnabled(true);
-		userAdmin.setName("admin");
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		userAdmin.setPassword(encoder.encode("admin"));
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleAdmin);
-		roles.add(roleUser);
-		userAdmin.setRoles(roles);
-		userRepository.save(userAdmin);
-		
-		Blog blogJavaVids = new Blog();
-		blogJavaVids.setName("JavaVids");
-		blogJavaVids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
-		blogJavaVids.setUser(userAdmin);
-		blogRepository.save(blogJavaVids);
-		
-		Item item1 = new Item();
-		item1.setBlog(blogJavaVids);
-		item1.setTitle("first");
-		item1.setLink("http://www.javavids.com");
-		item1.setPublishedDate(new Date());
-		itemRepository.save(item1);
-		
-		Item item2 = new Item();
-		item2.setBlog(blogJavaVids);
-		item2.setTitle("second");
-		item2.setLink("http://www.javavids.com");
-		item2.setPublishedDate(new Date());
-		itemRepository.save(item2);
-		
-	}
+				Role roleUser = new Role();
+				roleUser.setName("ROLE_USER");
+				roleRepository.save(roleUser);
+				
+				Role roleAdmin = new Role();
+				roleAdmin.setName("ROLE_ADMIN");
+				roleRepository.save(roleAdmin);
+				
+				User userAdmin = new User();
+				userAdmin.setEnabled(true);
+				userAdmin.setName("admin");
+				BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+				userAdmin.setPassword(encoder.encode("admin"));
+				List<Role> roles = new ArrayList<Role>();
+				roles.add(roleAdmin);
+				roles.add(roleUser);
+				userAdmin.setRoles(roles);
+				userRepository.save(userAdmin);
+				
+				Blog blogJavaVids = new Blog();
+				blogJavaVids.setName("JavaVids");
+				blogJavaVids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+				blogJavaVids.setUser(userAdmin);
+				blogRepository.save(blogJavaVids);
+				
+			/*	Item item1 = new Item();
+				item1.setBlog(blogJavaVids);
+				item1.setTitle("first");
+				item1.setLink("http://www.javavids.com");
+				item1.setPublishedDate(new Date());
+				itemRepository.save(item1);
+				
+				Item item2 = new Item();
+				item2.setBlog(blogJavaVids);
+				item2.setTitle("second");
+				item2.setLink("http://www.javavids.com");
+				item2.setPublishedDate(new Date());
+				itemRepository.save(item2);*/
+			}
+		}
 }
--- java-blog-aggregator/src/main/webapp/WEB-INF/jsp/index.jsp
+++ java-blog-aggregator/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,4 +2,36 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
-Hello from Spring Web MVC 123
+<%@ include file="../layout/taglib.jsp"%>
+
+
+<h1> Latest news from the Java world:</h1>
+
+<table class="table table-bordered table-hover table-striped">
+		<thead>
+			<tr>
+				<th>date</th>
+				<th>item</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach items="${items}" var="item">
+				<tr>
+					<td>
+						<c:out value="${item.publishedDate}" />
+						<br/> 
+						<c:out value="${item.blog.name}"/>
+					</td>
+					<td>
+						<strong>
+							<a href="<c:out value="${item.link}" />" target="_blank">
+								<c:out value="${item.title}" />
+							</a>
+						</strong>
+						<br />
+						${item.description}
+					</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
